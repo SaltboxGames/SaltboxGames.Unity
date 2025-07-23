c153a1a536274493dac41e7df1d85d6460b99668
Jonathan Gardner <Jon.Gardner@saltboxgames.com>
2025-07-22 17:12:07 -0700

    added log utils

---
+++ Runtime/Utilities/LogUtils.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Copyright (c) 2024 SaltboxGames, Jonathan Gardner
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+namespace SaltboxGames.Unity.Utilities
+{
+    public class LogUtils
+    {
+        [HideInCallstack]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static string Color(string message, Color color)
+        {
+#if UNITY_EDITOR
+            return $"<color=#{ColorUtility.ToHtmlStringRGB(color)}>{message}</color>";
+#else
+            return message;
+#endif
+        }
+    }
+}
