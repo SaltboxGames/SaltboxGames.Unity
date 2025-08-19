45f7e45cafd8d50dc755fda9fb466be39bd83f61
Jonathan Gardner <Jon.Gardner@saltboxgames.com>
2025-08-19 15:47:40 -0700

    added prefab pool

---
+++ Runtime/Collections/PoolableObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace SaltboxGames.Unity.Collections
+{
+    public class PoolableObject : MonoBehaviour
+    {
+        [NonSerialized]
+        internal PrefabPool Owner;
+
+        public event Action OnSpawned;
+        public event Action OnDespawned;
+
+        internal void InvokeSpawn()
+        {
+            OnSpawned?.Invoke();
+        }
+
+        internal void InvokeDespawned()
+        {
+            OnDespawned?.Invoke();
+        }
+        
+        public void Despawn()
+        {
+            Owner.Return(this);
+        }
+    }
+}
